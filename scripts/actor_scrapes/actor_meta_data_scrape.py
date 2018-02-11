cef21bd5a466f279b97b39f2ab28f1271532ad8a
ewlin <ericwilliamlin@gmail.com>
2018-02-11 00:14:22 -0500

    actor metadata scrape updates

--- scripts/actor_scrapes/actor_meta_data_scrape.py
+++ scripts/actor_scrapes/actor_meta_data_scrape.py
@@ -14,7 +14,7 @@ def get_actor_info(actor_meta):
     # write to a different file instead:
     # e.g., data_file = open('data/actors_meta/master_actors_list.tsv', 'a')
 
-    data_file = open('data/uk_productions/uk-performers-with-bday-better.tsv', 'a')
+    data_file = open('data/actors_test.tsv', 'a')
 
     actor_info = '\t'.join(actor_meta)
 
@@ -29,12 +29,12 @@ def get_actor_info(actor_meta):
         if not person_not_found:
             #birth_date = re.search(r'(\d+\s\w+\s\d\d\d\d|\w+\s\d+\,\s\d\d\d\d)', soup.get_text())
             #better birthday search: looks for (born 12 October 1987) or (born January 23, 1980) or (born 1970)
-            birth_date = re.search(r'\(born\s(\d+\s\w+\s\d\d\d\d|\w+\s\d+\,\s\d\d\d\d|\d\d\d\d)', soup.find('p').get_text())
+            birth_date = re.search(r'\(born\s(\d+\s\w+\s\d{4}|\w+\s\d+\,\s\d{4}|\d{4})', soup.find('p').get_text())
             if birth_date:
                 #print(actor_name, birth_date.group(0))
                 actor_info = birth_date.group(1) + '\t' + actor_info
             else:
-                birth_date = re.search(r'(\d+\s\w+\s\d\d\d\d|\w+\s\d+\,\s\d\d\d\d)', soup.find('p').get_text())
+                birth_date = re.search(r'(\d+\s\w+\s\d{4}|\w+\s\d+\,\s\d{4})', soup.find('p').get_text())
                 if birth_date:
                     actor_info = birth_date.group(0) + '\t' + actor_info
                 else:
@@ -49,7 +49,7 @@ def get_actor_info(actor_meta):
 #use wiki to scrape for gender (if first paragraph uses 'she' or 'her')
 #scrape for ethnicity?
 
-with open('data/uk_productions/uk-performers.tsv') as actors:
+with open('data/old_data/uk-performers.tsv') as actors:
     actors = unicodecsv.reader(actors, delimiter='\t')
     #for actor in actors:
         #get_actor_info(actor)
